fb1766f253f962f8a23df0737f16a9b240981f76
Lodewikus2000 <leoschreuders@hotmail.com>
2020-03-12 22:26:35 +0100

    knopjes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 // When the user scrolls the page, execute myFunction
 window.onload=function() {
 
-    
 
 
 
+    var h = document.getElementById('navbarbox').offsetHeight;
+    var bars = document.getElementsByClassName("boxbar");
+    console.log(bars);
+
+    Array.prototype.forEach.call(bars, function(bar) {
+    // Do stuff here
+    console.log(h);
+    bar.style.top = h + "px";
+    console.log(bar);
+    });
 
 }

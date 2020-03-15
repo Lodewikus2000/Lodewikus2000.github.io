cbb282bcd32d0d0ac6254b1f2c8d40ca0ace8295
Lodewikus2000 <leoschreuders@hotmail.com>
2020-03-15 22:46:25 +0100

    footer gefixt

--- js/script.js
+++ js/script.js
@@ -10,9 +10,19 @@ window.onload=function() {
         var h = document.getElementById('navbar').clientHeight;
         var bars = document.getElementsByClassName("header_box");
         Array.prototype.forEach.call(bars, function(bar) {
-            // Do stuff here
+
             bar.style.top = h - 1 + "px";
 
         });
     };
+
+
+    // const title_box = document.getElementById('title_box');
+    //
+    // title_box.onmouseover = function() {
+    //     title_box.style.animation ="headerHover 0.5s linear, headerHover2 0.25s linear 0.5s";
+    // }
+
+
+
 };
